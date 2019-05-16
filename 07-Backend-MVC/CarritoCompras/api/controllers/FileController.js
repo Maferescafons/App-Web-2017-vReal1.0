3385fc2ffd8e172d7dd182b3df3cd6c525b38e0b
Maferescafons <escafons@hotmail.com>
2019-05-16 15:48:57 -0500

    cambiando ip cama que coja los metodos de subir artículo

--- 07-Backend-MVC/CarritoCompras/api/controllers/FileController.js
+++ 07-Backend-MVC/CarritoCompras/api/controllers/FileController.js
@@ -10,7 +10,7 @@ module.exports = {
 
     res.writeHead(200, {'content-type': 'text/html'});
     res.end(
-      '<form action="http://localhost:1337/file/upload" enctype="multipart/form-data" method="post">'+
+      '<form action="http://http://35.198.47.176:8081/file/upload" enctype="multipart/form-data" method="post">'+
       '<input type="text" name="title"><br>'+
       '<input type="file" name="avatar" multiple="multiple"><br>'+
       '<input type="submit" value="Upload">'+
